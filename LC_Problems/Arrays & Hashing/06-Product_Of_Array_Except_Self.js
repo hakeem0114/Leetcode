cb8e5d34b1cd57faa994f500546e64c8fe6761eb
hakeem <hakeem0114@gmail.com>
2023-04-12 23:58:08 -0400

    Arrays & Hashing

---
+++ LC_Problems/Arrays & Hashing/06-Product_Of_Array_Except_Self.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let forwardArr  = []
+    let start = 1 //Since we are multiplying, 1 should be the start val
+
+    for(let i=0; i<nums.length;i++){
+        
+    }
+    
+};
